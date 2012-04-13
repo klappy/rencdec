815f6e952afa0c12003b2756c79c9de5896d9bfe
Christopher Klapp <christopher@klapp.name>
2012-04-13 10:24:43 -0400

    rendec class to encode, decode and verify. even works with a word represented as a symbol for each element in the arrays.

--- lib/rencdec.rb
+++ lib/rencdec.rb
@@ -1,15 +1,24 @@
 require 'radix'
 
-def rencdec(params)
-	current_encoding, target_encoding, current_data = params[:current_encoding], params[:target_encoding], params[:current_data]
-	current_base = Radix::Base.new current_encoding
-	target_base = Radix::Base.new target_encoding
-	target_data = target_base.convert current_data.join, current_base
-	puts "verified" if current_data == current_base.convert(target_data, target_base).chars.map(&:to_sym)
-	target_data
-end
+class Rencdec
+	attr_reader :verbose, :current_encoding, :target_encoding, :current_data, :current_radix, :target_radix, :target_data
+	def initialize(params)
+		@current_encoding, @target_encoding, @current_data = params[:current_encoding], params[:target_encoding], params[:current_data]
 
-puts rencdec(current_data: [:h,:e,:l,:l,:o],
-		current_encoding: [:a,:b,:c,:d,:e,:f,:g,:h,:i,:j,:k,:l,:m,:n,:o,:p,:q,:r,:s,:t,:u,:v,:w,:x,:y,:z],
-		target_encoding: 0..9)
-puts rencdec(current_data: [:q,:u,:e,:r,:t,:y], current_encoding: [:e,:q,:r,:t,:u,:y], target_encoding: [:a,:b,:c,:d])
+		@current_radix = Radix::Base.new current_encoding
+		@target_radix = Radix::Base.new target_encoding
+	end
+	def encode
+		tmp_current = current_data.map{|c| current_radix.values[c.to_s]}
+		tmp_target = Radix.convert_base(tmp_current, current_radix.base, target_radix.base)
+		@target_data ||= tmp_target.map{|t| target_radix.values.invert[t]}
+	end
+	def decode
+		tmp_target = target_data.map{|t| target_radix.values[t.to_s]}
+		tmp_current = Radix.convert_base(tmp_target, target_radix.base, current_radix.base)
+		@current_data ||= tmp_current.map{|t| current_radix.values.invert[t]}
+	end
+	def verify
+		encode == target_data && decode == current_data
+	end
+end
